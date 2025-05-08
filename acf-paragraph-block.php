8fa35af169b1ef526b74bcffcfd9f6ec490f30b6
Benjamin Vankat <bvankat@gmail.com>
2025-05-08 09:59:31 -0500

    Updated both files

--- acf-paragraph-block.php
+++ acf-paragraph-block.php
@@ -2,52 +2,49 @@
 /**
  * Plugin Name: ACF Paragraph Block
  * Description: Adds a custom paragraph block variation that pulls content from an ACF field.
- * Version: 1.0
+ * Version: 1.1
  * Author: Ben Vankat, Hanscom Park Studio
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-function acf_paragraph_block_register() {
-	// Register the block variation for Paragraph block
-	register_block_variation( 'core/paragraph', [
-		'name'        => 'acf-field',
-		'title'       => __( 'ACF Field Paragraph', 'acf-paragraph-block' ),
-		'description' => __( 'A Paragraph block that displays content from a specified ACF field.', 'acf-paragraph-block' ),
-		'attributes'  => [
-			'acfFieldSlug' => '',
-		],
-		'render_callback' => 'acf_paragraph_block_render',
-	]);
-}
-
-add_action( 'init', 'acf_paragraph_block_register' );
-
-function acf_paragraph_block_render( $attributes ) {
-	$acf_field_slug = isset( $attributes['acfFieldSlug'] ) ? $attributes['acfFieldSlug'] : '';
-	
-	if ( ! empty( $acf_field_slug ) ) {
-		// Get the value of the ACF field
-		$acf_value = get_field( $acf_field_slug );
-
-		// Return the field value wrapped in a paragraph element
-		return '<p>' . esc_html( $acf_value ) . '</p>';
-	}
-
-	return '<p>' . __( 'No ACF field found.', 'acf-paragraph-block' ) . '</p>';
-}
-
-// Enqueue the JavaScript for block registration
-function acf_paragraph_block_enqueue_script() {
-	wp_enqueue_script(
-		'acf-paragraph-block-js',
-		plugin_dir_url( __FILE__ ) . 'block.js',
-		array( 'wp-blocks', 'wp-editor', 'wp-components' ),
-		null,
-		true
-	);
-}
-
-add_action( 'enqueue_block_editor_assets', 'acf_paragraph_block_enqueue_script' );
+ 
+ if ( ! defined( 'ABSPATH' ) ) {
+	 exit; // Exit if accessed directly.
+ }
+ 
+ // Enqueue the JavaScript for block registration
+ function acf_paragraph_block_enqueue_script() {
+	 wp_enqueue_script(
+		 'acf-paragraph-block-js',
+		 plugin_dir_url( __FILE__ ) . 'block.js',
+		 array( 'wp-blocks', 'wp-editor', 'wp-components', 'wp-element' ),
+		 filemtime( plugin_dir_path( __FILE__ ) . 'block.js' ),
+		 true
+	 );
+ }
+ 
+ add_action( 'enqueue_block_editor_assets', 'acf_paragraph_block_enqueue_script' );
+ 
+ // Dynamic block render callback
+ function acf_paragraph_block_dynamic_render( $attributes ) {
+	 if ( isset( $attributes['acfFieldSlug'] ) && function_exists( 'get_field' ) ) {
+		 $acf_value = get_field( $attributes['acfFieldSlug'] );
+		 if ( $acf_value ) {
+			 return '<p>' . esc_html( $acf_value ) . '</p>';
+		 }
+	 }
+	 return '<p>' . esc_html__( 'No ACF field value found.', 'acf-paragraph-block' ) . '</p>';
+ }
+ 
+ // Register the dynamic block (tied to our variation)
+ function acf_paragraph_block_register_dynamic() {
+	 register_block_type( 'acf-paragraph-block/dynamic', array(
+		 'render_callback' => 'acf_paragraph_block_dynamic_render',
+		 'attributes' => array(
+			 'acfFieldSlug' => array(
+				 'type' => 'string',
+				 'default' => '',
+			 ),
+		 ),
+	 ) );
+ }
+ 
+ add_action( 'init', 'acf_paragraph_block_register_dynamic' );
--- block.js
+++ block.js
@@ -1,8 +1,10 @@
 const { registerBlockVariation } = wp.blocks;
 const { TextControl } = wp.components;
+const { InspectorControls } = wp.blockEditor;
+const { Fragment } = wp.element;
 
-registerBlockVariation( 'core/paragraph', {
-	name: 'acf-field',
+registerBlockVariation( 'acf-paragraph-block/dynamic', {
+	name: 'acf-field-paragraph',
 	title: 'ACF Field Paragraph',
 	description: 'A Paragraph block that displays content from an ACF field.',
 	attributes: {
@@ -11,22 +13,29 @@ registerBlockVariation( 'core/paragraph', {
 			default: '',
 		},
 	},
+	scope: [ 'inserter' ],
 	edit: ( props ) => {
 		const { attributes, setAttributes } = props;
-
 		return (
-			<>
-				<TextControl
-					label="ACF Field Slug"
-					value={ attributes.acfFieldSlug }
-					onChange={ ( value ) => setAttributes( { acfFieldSlug: value } ) }
-					help="Enter the slug of the ACF field to display in this paragraph."
-				/>
-			</>
+			<Fragment>
+				<InspectorControls>
+					<TextControl
+						label="ACF Field Slug"
+						value={ attributes.acfFieldSlug }
+						onChange={ ( value ) => setAttributes( { acfFieldSlug: value } ) }
+						help="Enter the slug of the ACF field to display."
+					/>
+				</InspectorControls>
+				<p style={{ border: '1px dashed #ccc', padding: '8px' }}>
+					{ attributes.acfFieldSlug 
+						? `This will display the ACF field: ${attributes.acfFieldSlug}` 
+						: 'Please set an ACF field slug in the block settings.' }
+				</p>
+			</Fragment>
 		);
 	},
 	save: () => {
-		// This block is dynamic, so we don't need to save anything here.
+		// Dynamic block: content rendered on server
 		return null;
 	},
 });
